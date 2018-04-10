0b06d9cc5e2965dc7f289727851405dc8d7d795f
stankant <stankovsky.ant@gmail.com>
2018-04-10 19:17:10 +0300

    updated  push notification data

--- utils/fcm.js
+++ utils/fcm.js
@@ -5,11 +5,7 @@ const fcm = new FCM(process.env.FCM_KEY);
 const createMessage = (to, message) => ({
   registration_ids: to,
   collapse_key: process.env.COLLAPSE_KEY,
-  data: message || 'Shultz!',
-  notification: {
-    title: 'Shultz!',
-    body: message || 'Shultz!'
-  }
+  data: message || 'Shultz!'
 });
 
 const sendMessage = async (to, message) => {
